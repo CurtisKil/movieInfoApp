3d5f6c4dad066f3a1a94dcd969c367c6085cdff8
lucidck <curtiskilgore@hotmail.com>
2020-08-01 12:18:53 -0700

    Added CSS/Responsiveness to Home Page

--- js/main.js
+++ js/main.js
@@ -13,7 +13,21 @@ function getMovies(searchText) {
     .get("http://www.omdbapi.com?s=" + searchText + "&apikey=7ba9f7e6")
     .then((response) => {
       console.log(response);
+      let movies = response.data.Search;
+      let output = "";
+      $.each(movies, (index, movie) => {
+        output += `
+        <div class="col-md-3">
+        <div class well text-center>
+        <img src="${movie.Poster}"/>
+        <h5>${movie.Title}</h5>
+        <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+        </div>
+        </div>`;
+      });
+      $("#movies").html(output);
     })
+
     .catch((err) => {
       console.log(err);
     });
